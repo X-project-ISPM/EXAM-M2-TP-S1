463fae521aeaeecb873e00e354e63ea8a3155d8c
Naily07 <leonelherman22@gmail.com>
2026-03-24 16:31:58 +0300

    refactor path data/corpus

--- backend/app/modules/lemmatizer.py
+++ backend/app/modules/lemmatizer.py
@@ -6,7 +6,7 @@ class Lemmatizer:
         # Configuration des chemins selon votre structure 
         base_dir = Path(__file__).resolve().parent
         # data_path = (base_dir / "../../../data/corpus/dictionary.json").resolve()
-        data_path = (base_dir / "../data/corpus/dictionary.json").resolve()
+        data_path = (base_dir / "../../data/corpus/dictionary.json").resolve()
 
         try:
             with open(data_path, "r", encoding="utf-8") as f:
